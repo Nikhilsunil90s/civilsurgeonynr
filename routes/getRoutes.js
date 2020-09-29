31cc781fbb4fd75248d951b19b6f181a819bf4e0
unknown <nikhilsunil90s@gmail.com>
2020-09-29 13:21:14 +0530

    email checked

--- routes/getRoutes.js
+++ routes/getRoutes.js
@@ -23,7 +23,9 @@ const options = {
 var nodemailer = require('nodemailer');
 
 var transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: 'csoynr@gmail.com',
     pass: 'csoynr123'
